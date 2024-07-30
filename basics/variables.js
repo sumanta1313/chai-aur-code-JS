7f5f4e542cd44f759dd279141493ecc64516c1d5
sumanta1313 <102575280+sumanta1313@users.noreply.github.com>
2024-07-30 17:12:36 +0000

    new file commited

---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accID = 14454
+let accEmail = "sam@122"
+var  accNum = "1233"
+accCity ="jaipur"
+let accHolder;
+
+
+
+// accID = 2
+console.log(accID);
+
+accEmail = "huhu@1233"
+accNum = "3211"
+accCity = "bengal"
+
+console.table([accEmail,accID,accNum,accCity,accHolder])
