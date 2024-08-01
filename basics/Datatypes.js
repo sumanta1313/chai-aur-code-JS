b7fa27cf7f28391c517d930f6fa64827bfbe0d23
sumanta1313 <102575280+sumanta1313@users.noreply.github.com>
2024-08-01 15:20:22 +0000

    Datatypes

---
+++ basics/Datatypes.js
@@ -0,0 +1,22 @@
+//"use strict"; //treat all js code like a newer version 
+
+// alert(3+3) // we are using node js not browser
+
+
+let name = "sumanta"
+let age = 22
+let start;
+let data = true
+
+
+console.log(typeof data);
+console.log(typeof age);
+console.log(typeof start);
+console.log(typeof "sumanta");
+console.log(typeof null); // it returns object 
+console.log(typeof undefined); //returns bcz its a type 
+
+
+
+
+
